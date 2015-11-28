8963b4333d537f43ad6589bd5f3ab04940b80e19
Mohammed Rizvi <morizvi07@gmail.com>
2015-11-28 17:03:44 -0500

    Fix jquery so that location would show in the hotel results

--- js/hotelsite.js
+++ js/hotelsite.js
@@ -1,15 +1,28 @@
+
 $(document).ready(function(){
 
   
   //Body panels hide
   $(".hotel-search").hide();
 
+  //AlertDanger hide
+  $(".alert-danger").hide();
 
   //Destination Name Search
   $(".btn-default").on("click", function(e){
     e.preventDefault();
+    var city = $("#location").val().trim();
+    var checkIn = $("#checkin").val().trim();
+    var checkOut = $("#checkout").val().trim();
+    if (city === "" || checkIn === "" || checkOut === ""){
+      $(".alert-danger").slideDown(500);
+      return;
+    };
+    
 
     var location = $("#location").val().trim();
+
+   
 
     // Name of destination appears
     var destinationLocation = $(".hotel-location");
@@ -18,5 +31,16 @@ $(document).ready(function(){
     $(".hotel-search").fadeIn(500);
 
   });
-  
+
+  //Clearing alert
+  $("#location").on("click keydown mouseover", function() {
+    $(".alert-danger").fadeOut();
+  });
+  $("#checkin").on("mouseover", function() {
+    $(".alert-danger").fadeOut();
+  });
+  $("#checkout").on("mouseover", function() {
+    $(".alert-danger").fadeOut();
+  });
+
 });
