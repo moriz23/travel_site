3d5a22e24f381f52d14f2a2079520872b88e19aa
C-Das <chinmay98@gmail.com>
2015-11-28 09:55:30 -0500

    Added Modal HTML for search

--- js/hotelsite.js
+++ js/hotelsite.js
@@ -1,1 +1,10 @@
 //alert ("Hi java script file is linked");
+$(document).ready (function (){
+  $(".alerts").hide();
+
+  $("#citySubmit").on ("click",function(e){
+    e.preventDefault(); // Without prevent default on submit button, Modal was not launched
+
+  }); //End of City Submit funtion, which calls the Modal to get input
+
+});
